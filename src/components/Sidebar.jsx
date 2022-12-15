c13dca7cacbf45d914769517c0ad740849bcd6b6
Borkkris <Borkkris@users.noreply.github.com>
2022-12-15 21:49:06 +0100

    Update Sidebar.jsx
    created a sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,13 +6,18 @@ import { Link, NavLink } from 'react-router-dom';
 import { SiShopware } from 'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
 
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { links } from '../data/dummy';
 
+import { useStateContext } from '../contexts/ContextProvider';
+
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu} = useStateContext();
+
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray dark:text-gray-200 dark:hover:text-red hover:bg-light-gray m-2'
 
   return (
     <div className="ml-3 h-screen 
@@ -22,14 +27,52 @@ const Sidebar = () => {
       {activeMenu && (<> 
         <div className="flex justify-between items-center">
           <Link to="/" onClick={() =>{}} 
-          // tailwind classes for the Link in the Menu
-          className="item-center gap-3 ml-3 mt-4 
-          flex text-xl font-extrabold tracking-tight
-          dark:text-white text-slate-900"
+            // tailwind classes for the Link in the Menu
+            className="items-center gap-3 ml-3 mt-4 
+            flex text-xl font-extrabold tracking-tight
+            dark:text-white text-slate-900"
           >
+            {/* icon */}
             <SiShopware /> <span>Shoppy</span>
           </Link>
-          
+          <TooltipComponent content="Menu" position="BottomCenter">
+            <button 
+              type="button"
+              // clicking lets the Sidebar dissapear
+              onClick={()=> setActiveMenu(false)}
+              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+            >
+                {/* icon */}
+                <MdOutlineCancel />
+            </button>
+          </TooltipComponent>
+        </div>
+        {/* actual items placed in this div */}
+        <div className="mt-10">
+          {links.map((item) => (
+            <div key={item.title}>
+              <p className="text-gray-400 m-3 mt-4 uppercase">
+                {item.title}
+              </p>
+              {item.links.map((Link) => (
+                <NavLink 
+                  to={`/${Link.name}`}
+                  key={Link.name}
+                  onClick={()=> setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+                  // className with function with a state (destructured)
+                  // to render different classnames
+                  // isActive is a boolean
+                  className={({ isActive }) => 
+                  isActive ? activeLink : normalLink}
+                >
+                  {Link.icon}
+                  <span className="capitalize">
+                    {Link.name}
+                  </span>
+                </NavLink>
+              ))}
+            </div>
+          ))}
         </div>
       </>)}
     </div>
