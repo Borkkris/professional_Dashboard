70bb345b671799ac4ea87976d6f8a546829cf9de
Borkkris <Borkkris@users.noreply.github.com>
2022-12-15 21:49:28 +0100

    Update index.js
    wrapped the app with ContextProvider

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App'
 
+import { ContextProvider } from './contexts/ContextProvider';
+
 // hook our react application to the root <div>
-ReactDOM.render(<App />, document.getElementById('root'));
+// wrap the app with the ContextProvider
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>, 
+    document.getElementById('root')
+);
