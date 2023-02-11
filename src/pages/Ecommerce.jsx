338f9abcdfa8f18dfef23b6d529e116585411666
Borkkris <Borkkris@users.noreply.github.com>
2023-02-11 18:13:06 +0100

    edited footer and ecommerce site

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -196,7 +196,6 @@ const Ecommerce = () => {
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl w-96 md:w-760">
           <div className="flex justify-between items-center gap-2 mb-10">
             <p className="text-xl font-semibold">Sales Overview</p>
-            <DropDown currentMode={currentMode} />
           </div>
           <div className="md:w-full overflow-auto">
             <LineChart />
